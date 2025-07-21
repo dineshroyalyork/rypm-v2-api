d5635c541dd0e4aed38c538a26ee357c1887135f
Amardeep kumar <amardeep.kumar@viithiisys.com>
2025-07-21 22:23:35 +0530

    add data

--- src/modules/account-information/controllers/account-information.controller.ts
+++ src/modules/account-information/controllers/account-information.controller.ts
@@ -13,28 +13,15 @@ export class AccountInformationController {
   @Post()
   @UsePipes(new ZodValidationPipe(accountInformationSchema))
   async createOrUpdateAccountInformation(@Request() req: any, @Body() accountInformationDto: AccountInformationDto) {
-    const tenantId = '9f5c342a-ac58-42b7-ade9-7f0382d238ca'; //req.user.id;
+    const tenantId = req.user.id;
     const result = await this.accountInformationService.createOrUpdateAccountInformation(tenantId, accountInformationDto);
     return result;
   }
 
-  @Get('get')
+  @Get()
   async getAccountInformation(@Request() req: any, @Query('type') type?: InformationType) {
-    try {
-      const tenantId = req.user.id;
-      const result = await this.accountInformationService.getAccountInformation(tenantId, type);
-
-      return {
-        success: true,
-        message: type ? `${type.replace('_', ' ')} retrieved successfully` : 'All account information retrieved successfully',
-        data: result,
-      };
-    } catch (error) {
-      return {
-        success: false,
-        message: error.message,
-        data: null,
-      };
-    }
+    const tenantId = req.user.id;
+    const result = await this.accountInformationService.getAccountInformation(tenantId, type);
+    return result;
   }
 }
