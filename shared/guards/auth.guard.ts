9bc7cbc64d23bc179f6a02b3f208283f18824e11
Shubham Goel <shubhamgoel@Shubhams-MacBook-Air.local>
2025-07-25 21:16:01 +0530

    change in auth function

--- shared/guards/auth.guard.ts
+++ shared/guards/auth.guard.ts
@@ -29,7 +29,7 @@ export class AuthGuard implements CanActivate {
       );
       // 💡 We're assigning the payload to the request object here
       // so that we can access it in our route handlers
-      request['tenants'] = payload;
+      request['user'] = payload;
     } catch {
       throw new UnauthorizedException('Invalid or expired token');
     }
