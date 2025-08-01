c9e9406c3f09f7034074ec8a486c7108a17f4b85
Amardeep kumar <amardeep.kumar@viithiisys.com>
2025-08-01 17:24:07 +0530

    add data

--- src/modules/tour-scheduling/dto/create-tour-scheduled.dto.ts
+++ src/modules/tour-scheduling/dto/create-tour-scheduled.dto.ts
@@ -5,7 +5,7 @@ export const createTourScheduledSchema = z.object({
   agent_id: z.string().optional(),
   move_in_date: z.string().datetime('Invalid date format').optional(),
   monthly_income: z.number().min(0, 'Monthly income must be positive').optional(),
-  credit_score: z.number().min(300, 'Credit score must be at least 300').max(850, 'Credit score must be at most 850').optional(),
+  credit_score: z.string({ message: 'Credit score is in string format' }).optional(),
   source_of_income: z.string().optional(),
   occupation: z.string().optional(),
 });
